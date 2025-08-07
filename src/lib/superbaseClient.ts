9e178eab1465008993ef6056d9bde5a607c4caec
SABONGSEOK <qhdtjr4517@gmail.com>
2025-08-07 18:36:23 +0900

    서버리스 오류 수정 테스트

--- src/lib/superbaseClient.ts
+++ src/lib/superbaseClient.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+const isLocal = import.meta.env.VITE_ENV === 'local';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+const supabaseUrl = isLocal ? import.meta.env.VITE_SUPABASE_URL! : import.meta.env.SUPABASE_URL!;
+const supabaseAnonKey = isLocal
+  ? import.meta.env.VITE_SUPABASE_ANON_KEY!
+  : import.meta.env.SUPABASE_ANON_KEY!;
 console.log('supabaseUrl', supabaseUrl);
 console.log('supabaseAnonKey', supabaseAnonKey);
 
