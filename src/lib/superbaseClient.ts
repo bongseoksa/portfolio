7a387e9b8b0b639a80f21967e9e593bf08f928a0
SABONGSEOK <qhdtjr4517@gmail.com>
2025-08-07 19:14:28 +0900

    서버리스 환경변수 수정

--- src/lib/superbaseClient.ts
+++ src/lib/superbaseClient.ts
@@ -2,11 +2,13 @@ import { createClient } from '@supabase/supabase-js';
 
 const isLocal = import.meta.env.VITE_ENV === 'local';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+const supabaseUrl = isLocal ? import.meta.env.VITE_SUPABASE_URL! : process.env.SUPABASE_URL;
+const supabaseAnonKey = isLocal
+  ? import.meta.env.VITE_SUPABASE_ANON_KEY!
+  : process.env.SUPABASE_ANON_KEY;
 
-console.log('isLocal', isLocal);
-console.log('supabaseUrl', supabaseUrl);
-console.log('supabaseAnonKey', supabaseAnonKey);
+// console.log('isLocal', isLocal);
+// console.log('supabaseUrl', supabaseUrl);
+// console.log('supabaseAnonKey', supabaseAnonKey);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
