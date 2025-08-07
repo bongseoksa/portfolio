435530760ee58f63018cbb6a1e144701494548e2
SABONGSEOK <qhdtjr4517@gmail.com>
2025-08-07 19:18:12 +0900

    GET 메소드 오류 수정

--- src/lib/superbaseClient.ts
+++ src/lib/superbaseClient.ts
@@ -1,14 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const isLocal = import.meta.env.VITE_ENV === 'local';
-
-const supabaseUrl = isLocal ? import.meta.env.VITE_SUPABASE_URL! : process.env.SUPABASE_URL;
-const supabaseAnonKey = isLocal
-  ? import.meta.env.VITE_SUPABASE_ANON_KEY!
-  : process.env.SUPABASE_ANON_KEY;
-
-// console.log('isLocal', isLocal);
-// console.log('supabaseUrl', supabaseUrl);
-// console.log('supabaseAnonKey', supabaseAnonKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
